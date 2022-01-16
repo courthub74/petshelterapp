5cbf1d2efd4f2a643317b485e7868989c47e96a2
Courdevelops <courdevelops@gmail.com>
2022-01-16 18:53:09 -0500

    reactdefault

--- views/home.jsx
+++ views/home.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import DefaultLayout from './layouts/default'
 
 
 function Home(props) {
     return (
-        <h1>Home Page</h1>
+        // <h1>Home Page</h1>
+        <DefaultLayout title="Home Page">
+            <h1>Default Layout Homepage </h1>
+            { props.data }
+        </DefaultLayout>
     )
 }
 
