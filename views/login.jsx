7755955ecb1b98b7fa0958e474e85c1b126534dd
Courdevelops <courdevelops@gmail.com>
2022-01-21 14:14:29 -0500

    emailpassword

--- views/login.jsx
+++ views/login.jsx
@@ -7,7 +7,26 @@ const Login = (props) => {
     return (
         <DefaultLayout>
             <Nav />
-            <h1>Login Page Here</h1>
+            <form
+                //Required
+                action="/login"
+                method='POST' //overwrite the default GET method
+                encType='multipart/form-data'
+                //Optional
+                class="cat-form" 
+                >
+                <h2>Login</h2>
+
+                <div className=''>
+                    <input type="text" name="email" placeholder='Add email here....' />
+                </div>
+
+                <div className=''>
+                    <input type="text" name="password" placeholder='Add password....' />
+                </div>
+
+                <button type="submit">Login</button>
+            </form>
             <Footer />
         </DefaultLayout>
     )
